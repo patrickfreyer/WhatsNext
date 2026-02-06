d49012518e342f86fb8b2d6fc186be2bcfc0ee7e
patrickfreyer <mail@patrickfreyer.com>
2026-02-06 10:29:17 +0100

    Update WhatsNext/ViewModels/MenuBarViewModel.swift via Claude Code

--- WhatsNext/ViewModels/MenuBarViewModel.swift
+++ WhatsNext/ViewModels/MenuBarViewModel.swift
@@ -2,6 +2,27 @@ import Combine
 import Foundation
 import SwiftUI
 
+// MARK: - Debug Logger
+
+func debugLog(_ message: String) {
+    let logFile = FileManager.default.homeDirectoryForCurrentUser.appendingPathComponent("whatsnext_debug.log")
+    let timestamp = ISO8601DateFormatter().string(from: Date())
+    let logMessage = "[\(timestamp)] \(message)\n"
+
+    if let data = logMessage.data(using: .utf8) {
+        if FileManager.default.fileExists(atPath: logFile.path) {
+            if let handle = try? FileHandle(forWritingTo: logFile) {
+                handle.seekToEndOfFile()
+                handle.write(data)
+                handle.closeFile()
+            }
+        } else {
+            try? data.write(to: logFile)
+        }
+    }
+    NSLog("[WhatsNext] %@", message)
+}
+
 // MARK: - Menu Bar View Model
 
 @MainActor
